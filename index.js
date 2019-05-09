5f074d3be7a43cb4ff6fb0d9755b56208ed6081e
Zach Overhulser <zoverhulser@gmail.com>
2019-05-09 16:29:11 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(process.env.PORT, () => {
+  console.log(`Server Running on ${process.env.PORT}`);
 });
